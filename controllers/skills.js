909fd8fadb76fb4d51525b6449a57507a80e97fc
Leon Chu <Ljchu87@gmail.com>
2022-09-26 17:34:37 -0500

    Refactor index controller function

--- controllers/skills.js
+++ controllers/skills.js
@@ -1,9 +1,17 @@
-// Import the skills data
+// Import the model that we exported in the Todo.js model file
 import { skills } from '../data/skills-data.js'
+import { Skill } from '../models/skill.js'
 
 function index(req, res) {
-  res.render('skills/index', {
-    skills: skills
+  Skill.find({})
+  .then(skills => {
+    res.render('skills/index', {
+      skills: skills
+    })
+  })
+  .catch(error => {
+    console.log(error)
+    res.redirect('/')
   })
 }
 
